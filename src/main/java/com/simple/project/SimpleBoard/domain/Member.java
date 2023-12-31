d5dc9e9fec5b29c35e75ab36fa4d9f964284c622
BaeDongHyeon <sasd730@gmail.com>
2023-12-31 12:53:07 +0900

    ADD: Member domain add

---
+++ src/main/java/com/simple/project/SimpleBoard/domain/Member.java
@@ -0,0 +1,20 @@
+package com.simple.project.SimpleBoard.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+}
