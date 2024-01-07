7a02c5c68e357afe06246b2181137bbb9dd3d245
BaeDongHyeon <sasd730@gmail.com>
2024-01-07 09:23:11 +0900

    REFACTOR: mapping add, service connect - 로그인, 회원가입 페이지 Member 클래스 전달 - Post mapping 추가 - 회원가입 서비스 연결

--- src/main/java/com/simple/project/SimpleBoard/controller/HomeController.java
+++ src/main/java/com/simple/project/SimpleBoard/controller/HomeController.java
@@ -1,12 +1,18 @@
 package com.simple.project.SimpleBoard.controller;
 
+import com.simple.project.SimpleBoard.domain.Member;
+import com.simple.project.SimpleBoard.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
+
+    private final MemberService memberService;
 
     @GetMapping("/")
     public String mainPage() {
@@ -14,17 +20,30 @@ public class HomeController {
     }
 
     @GetMapping("/login")
-    public String loginPage() {
+    public String loginPage(Model model) {
+        model.addAttribute("member", new Member());
         return "form/loginPage";
     }
 
     @GetMapping("/signup")
-    public String signupPage() {
+    public String signupPage(Model model) {
+        model.addAttribute("member", new Member());
         return "form/signupPage";
     }
 
     @GetMapping("/write")
     public String writePage() {
         return "form/writePage";
+    }
+
+    @PostMapping("/member/new")
+    public String signup(Member member) {
+        memberService.signupMember(member);
+        return "redirect:/login";
+    }
+
+    @PostMapping("/login")
+    public String login(Member member) {
+        return "redirect:/";
     }
 }
