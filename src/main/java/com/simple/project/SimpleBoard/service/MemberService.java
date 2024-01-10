e68172e3e2be3a41fab8fc44c6c5c3599609e74c
BaeDongHyeon <sasd730@gmail.com>
2024-01-10 20:46:00 +0900

    REFACTOR: 생성자를 어노테이션으로 대체

--- src/main/java/com/simple/project/SimpleBoard/service/MemberService.java
+++ src/main/java/com/simple/project/SimpleBoard/service/MemberService.java
@@ -2,16 +2,14 @@ package com.simple.project.SimpleBoard.service;
 
 import com.simple.project.SimpleBoard.domain.Member;
 import com.simple.project.SimpleBoard.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
-
-    public MemberService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
 
     public void signupMember(Member member) {
         memberRepository.save(member);
