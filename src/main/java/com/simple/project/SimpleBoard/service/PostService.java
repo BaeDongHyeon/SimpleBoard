3fb745ec2a2c202c2fe19f96806bad6af629604f
BaeDongHyeon <sasd730@gmail.com>
2024-01-11 17:47:46 +0900

    ADD: 작성된 게시글을 모두 호출하는 기능 구현

--- src/main/java/com/simple/project/SimpleBoard/service/PostService.java
+++ src/main/java/com/simple/project/SimpleBoard/service/PostService.java
@@ -5,6 +5,8 @@ import com.simple.project.SimpleBoard.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class PostService {
@@ -13,5 +15,9 @@ public class PostService {
 
     public void writePost(Post post) {
         postRepository.save(post);
+    }
+
+    public List<Post> findAllPosts() {
+        return postRepository.findAll();
     }
 }
