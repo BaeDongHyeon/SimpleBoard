77308c26107a0e3837db3830f16a3766e84113d2
BaeDongHyeon <sasd730@gmail.com>
2024-01-14 11:11:06 +0900

    ADD: insert 및 update가 발생하면 현재 시간을 채우도록 필드값 지정

--- src/main/java/com/simple/project/SimpleBoard/domain/Post.java
+++ src/main/java/com/simple/project/SimpleBoard/domain/Post.java
@@ -26,9 +26,9 @@ public class Post {
     private String detail;
 
     @CreationTimestamp
-    private LocalDateTime createdDate;
+    private LocalDateTime createdDate = LocalDateTime.now();
 
     @LastModifiedDate
-    private LocalDateTime updatedDate;
+    private LocalDateTime updatedDate = LocalDateTime.now();
 
 }
