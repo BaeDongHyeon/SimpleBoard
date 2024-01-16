234241d3429dca03de460b01381f75f46311dd9c
BaeDongHyeon <sasd730@gmail.com>
2024-01-16 18:22:24 +0900

    ADD: 임시 로그인 기능 구현

--- src/main/java/com/simple/project/SimpleBoard/service/MemberService.java
+++ src/main/java/com/simple/project/SimpleBoard/service/MemberService.java
@@ -1,16 +1,36 @@
 package com.simple.project.SimpleBoard.service;
 
 import com.simple.project.SimpleBoard.domain.Member;
+import com.simple.project.SimpleBoard.domain.dto.LoginRequest;
 import com.simple.project.SimpleBoard.domain.dto.MemberSaveRequest;
 import com.simple.project.SimpleBoard.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
+
+    public void login(LoginRequest loginRequest) {
+        Optional<Member> findEmailSameMember = memberRepository.findByEmail(loginRequest.getEmail());
+
+        Member member = null;
+        if (findEmailSameMember.isEmpty()) {
+            System.out.println("로그인 실패");
+        } else {
+            member = findEmailSameMember.get();
+        }
+
+        if (member.passwordMatch(loginRequest.getPassword())) {
+            System.out.println("로그인 성공");
+        } else {
+            System.out.println("로그인 실패");
+        }
+    }
 
     public void signupMember(MemberSaveRequest memberSaveRequest) {
         memberRepository.save(memberSaveRequest.toEntity());
