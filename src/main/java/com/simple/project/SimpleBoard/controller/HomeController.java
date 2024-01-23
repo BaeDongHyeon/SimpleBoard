87edbcc863777f8d2e9a9a9c6d312e39e83c31c5
BaeDongHyeon <sasd730@gmail.com>
2024-01-23 21:02:31 +0900

    ADD: 게시글 작성 페이지 매핑

--- src/main/java/com/simple/project/SimpleBoard/controller/HomeController.java
+++ src/main/java/com/simple/project/SimpleBoard/controller/HomeController.java
@@ -1,7 +1,9 @@
 package com.simple.project.SimpleBoard.controller;
 
+import com.simple.project.SimpleBoard.domain.form.PostForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,5 +13,11 @@ public class HomeController {
     @GetMapping("/")
     public String mainPage() {
         return "form/mainPage";
+    }
+
+    @GetMapping("/write")
+    public String writePage(Model model) {
+        model.addAttribute("postForm", new PostForm());
+        return "form/writePage";
     }
 }
