0e0e0d4a5f8ea8310d05faad9c6f23b75af70713
BaeDongHyeon <sasd730@gmail.com>
2024-01-23 21:56:07 +0900

    ADD: 글 작성 기능 Post 매핑

--- src/main/java/com/simple/project/SimpleBoard/controller/HomeController.java
+++ src/main/java/com/simple/project/SimpleBoard/controller/HomeController.java
@@ -1,10 +1,12 @@
 package com.simple.project.SimpleBoard.controller;
 
 import com.simple.project.SimpleBoard.domain.form.PostForm;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,5 +21,13 @@ public class HomeController {
     public String writePage(Model model) {
         model.addAttribute("postForm", new PostForm());
         return "form/writePage";
+    }
+
+    @PostMapping("/write")
+    public String write(@Valid PostForm postForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "form/writePage";
+        }
+        return "redirect:/";
     }
 }
