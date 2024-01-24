6ba4e36ad15a11fdd0c26873379b3a7a2c0de348
BaeDongHyeon <sasd730@gmail.com>
2024-01-24 11:42:52 +0900

    ADD: 게시글 정보를 담을 dto 생성

---
+++ src/main/java/com/simple/project/SimpleBoard/domain/response/PostSearchResponse.java
@@ -0,0 +1,33 @@
+package com.simple.project.SimpleBoard.domain.response;
+
+import com.simple.project.SimpleBoard.domain.Post;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class PostSearchResponse {
+
+    private Long id;
+    private String title;
+    private String writer;
+    private String content;
+
+    @Builder
+    public PostSearchResponse(Long id, String title, String writer, String content) {
+        this.id = id;
+        this.title = title;
+        this.writer = writer;
+        this.content = content;
+    }
+
+    public Post toEntity() {
+        return Post.builder()
+                .id(id)
+                .title(title)
+                .writer(writer)
+                .content(content)
+                .build();
+    }
+}
