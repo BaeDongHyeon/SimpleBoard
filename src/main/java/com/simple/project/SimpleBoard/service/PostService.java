9e2f784afead40d639434116b10dc2674701a0ab
BaeDongHyeon <sasd730@gmail.com>
2024-01-24 11:21:24 +0900

    ADD: 모든 게시글 조회 기능 추가

--- src/main/java/com/simple/project/SimpleBoard/service/PostService.java
+++ src/main/java/com/simple/project/SimpleBoard/service/PostService.java
@@ -6,11 +6,17 @@ import com.simple.project.SimpleBoard.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class PostService {
 
     private final PostRepository postRepository;
+
+    public List<Post> findAllPosts() {
+        return postRepository.findAll();
+    }
 
     public void savePost(PostForm postForm) {
         Post post = Post.builder()
