e5e4b3d5c7504f4deb924229a8cee4c511bb4054
BaeDongHyeon <sasd730@gmail.com>
2024-01-24 11:51:16 +0900

    REFACTOR: 반환타입을 Post -> PostSearchResponse로 변경

--- src/main/java/com/simple/project/SimpleBoard/service/PostService.java
+++ src/main/java/com/simple/project/SimpleBoard/service/PostService.java
@@ -2,11 +2,13 @@ package com.simple.project.SimpleBoard.service;
 
 import com.simple.project.SimpleBoard.domain.Post;
 import com.simple.project.SimpleBoard.domain.form.PostForm;
+import com.simple.project.SimpleBoard.domain.response.PostSearchResponse;
 import com.simple.project.SimpleBoard.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -14,8 +16,16 @@ public class PostService {
 
     private final PostRepository postRepository;
 
-    public List<Post> findAllPosts() {
-        return postRepository.findAll();
+    public List<PostSearchResponse> findAllPosts() {
+        List<Post> allPosts = postRepository.findAll();
+        return allPosts.stream()
+                .map(post -> PostSearchResponse.builder()
+                        .id(post.getId())
+                        .title(post.getTitle())
+                        .writer(post.getWriter())
+                        .content(post.getContent())
+                        .build())
+                .collect(Collectors.toList());
     }
 
     public void savePost(PostForm postForm) {
