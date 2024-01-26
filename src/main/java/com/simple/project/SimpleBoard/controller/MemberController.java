c602de88ecd0f61469be9faf2db302d5d080be60
BaeDongHyeon <sasd730@gmail.com>
2024-01-26 16:11:13 +0900

    ADD: 회원가입 서비스 연결 및 매핑

--- src/main/java/com/simple/project/SimpleBoard/controller/MemberController.java
+++ src/main/java/com/simple/project/SimpleBoard/controller/MemberController.java
@@ -2,10 +2,13 @@ package com.simple.project.SimpleBoard.controller;
 
 import com.simple.project.SimpleBoard.domain.form.MemberForm;
 import com.simple.project.SimpleBoard.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,5 +20,15 @@ public class MemberController {
     public String signupPage(Model model) {
         model.addAttribute("memberForm", new MemberForm());
         return "form/signupPage";
+    }
+
+    @PostMapping("/signup")
+    public String signup(@Valid MemberForm memberForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "form/signupPage";
+        }
+
+        memberService.saveMember(memberForm);
+        return "redirect:/";
     }
 }
