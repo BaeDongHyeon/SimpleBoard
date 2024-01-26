24ddd6c2444e25431a257f3910babbfa0290af91
BaeDongHyeon <sasd730@gmail.com>
2024-01-26 10:44:40 +0900

    ADD: 회원 domain 생성

---
+++ src/main/java/com/simple/project/SimpleBoard/domain/Member.java
@@ -0,0 +1,32 @@
+package com.simple.project.SimpleBoard.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    @Builder(builderMethodName = "createMember")
+    public Member(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+}
