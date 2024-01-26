15f09f3a2925a201d24e1c7ac22f592553290187
BaeDongHyeon <sasd730@gmail.com>
2024-01-26 15:47:02 +0900

    ADD: 회원 서비스 생성

---
+++ src/main/java/com/simple/project/SimpleBoard/service/MemberService.java
@@ -0,0 +1,14 @@
+package com.simple.project.SimpleBoard.service;
+
+import com.simple.project.SimpleBoard.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+
+}
