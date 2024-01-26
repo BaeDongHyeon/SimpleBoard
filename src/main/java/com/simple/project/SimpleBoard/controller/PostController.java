57be864ca8fd29d6614311d42e2ecb1b5ea70c85
BaeDongHyeon <sasd730@gmail.com>
2024-01-26 10:37:18 +0900

    REFACTOR: 컨트롤러명 변경

--- src/main/java/com/simple/project/SimpleBoard/controller/PostController.java
+++ src/main/java/com/simple/project/SimpleBoard/controller/PostController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
-public class HomeController {
+public class PostController {
 
     private final PostService postService;
 
