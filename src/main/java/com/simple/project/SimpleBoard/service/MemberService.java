5d9f10de3951946639c9f0642a7e123444a99641
BaeDongHyeon <sasd730@gmail.com>
2024-01-26 16:10:58 +0900

    ADD: 회원가입 서비스 추가

--- src/main/java/com/simple/project/SimpleBoard/service/MemberService.java
+++ src/main/java/com/simple/project/SimpleBoard/service/MemberService.java
@@ -1,5 +1,7 @@
 package com.simple.project.SimpleBoard.service;
 
+import com.simple.project.SimpleBoard.domain.Member;
+import com.simple.project.SimpleBoard.domain.form.MemberForm;
 import com.simple.project.SimpleBoard.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,5 +12,13 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    public void saveMember(MemberForm memberForm) {
+        Member member = Member.createMember()
+                        .email(memberForm.getEmail())
+                        .password(memberForm.getPassword())
+                        .name(memberForm.getName())
+                        .build();
 
+        memberRepository.save(member);
+    }
 }
