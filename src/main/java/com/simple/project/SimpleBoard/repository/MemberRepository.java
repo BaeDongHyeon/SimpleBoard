a0bafa2ac7cd17773805b96b3af94e223c638266
BaeDongHyeon <sasd730@gmail.com>
2024-01-29 15:50:04 +0900

    ADD: email을 조회하는 메소드 추가

--- src/main/java/com/simple/project/SimpleBoard/repository/MemberRepository.java
+++ src/main/java/com/simple/project/SimpleBoard/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package com.simple.project.SimpleBoard.repository;
 import com.simple.project.SimpleBoard.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
 }
