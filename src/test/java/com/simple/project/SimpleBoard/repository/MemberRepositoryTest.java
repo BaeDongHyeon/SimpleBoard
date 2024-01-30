4a6da9fab46096f6cc2cb0152f6030c4e5b71b56
BaeDongHyeon <sasd730@gmail.com>
2024-01-30 16:35:12 +0900

    test: 회원 번호로 회원 조회 테스트 코드 작성

--- src/test/java/com/simple/project/SimpleBoard/repository/MemberRepositoryTest.java
+++ src/test/java/com/simple/project/SimpleBoard/repository/MemberRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.simple.project.SimpleBoard.repository;
 
 import com.simple.project.SimpleBoard.domain.Member;
+import com.simple.project.SimpleBoard.domain.form.MemberForm;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,5 +34,31 @@ class MemberRepositoryTest {
         assertThat(saveMember.getName()).isEqualTo(member.getName());
     }
 
+    @Test
+    @DisplayName("회원 번호로 회원이 조회되어야 한다.")
+    void findMember_id() {
+        // given
+        Member member1 = Member.createMember()
+                .email("1@1.com")
+                .password("1111")
+                .name("홍길동1")
+                .build();
 
+        Member member2 = Member.createMember()
+                .email("2@2.com")
+                .password("2222")
+                .name("홍길동2")
+                .build();
+
+        Long memberId = memberRepository.save(member1).getId();
+        memberRepository.save(member2);
+
+        // when
+        Member findMember = memberRepository.findById(memberId).get();
+
+        // then
+        assertThat(findMember.getEmail()).isEqualTo(member1.getEmail());
+        assertThat(findMember.getPassword()).isEqualTo(member1.getPassword());
+        assertThat(findMember.getName()).isEqualTo(member1.getName());
+    }
 }
