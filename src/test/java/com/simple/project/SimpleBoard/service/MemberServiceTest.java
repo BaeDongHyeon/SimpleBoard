02d0f6e6cf5375f183eba6e8bfc409043f7639cc
BaeDongHyeon <sasd730@gmail.com>
2024-01-31 10:53:22 +0900

    test: 로그인 성공시 테스트 코드 작성

--- src/test/java/com/simple/project/SimpleBoard/service/MemberServiceTest.java
+++ src/test/java/com/simple/project/SimpleBoard/service/MemberServiceTest.java
@@ -2,6 +2,7 @@ package com.simple.project.SimpleBoard.service;
 
 import com.simple.project.SimpleBoard.domain.Member;
 import com.simple.project.SimpleBoard.domain.form.MemberForm;
+import com.simple.project.SimpleBoard.domain.request.MemberLoginRequest;
 import com.simple.project.SimpleBoard.repository.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -39,7 +40,29 @@ class MemberServiceTest {
     }
 
     @Test
+    @Transactional
+    @DisplayName("회원이 로그인이 성공되면 번호와 이름이 반환되어야 한다.")
     void loginMember() {
+        // given
+        MemberForm memberForm = MemberForm.builder()
+                .email("abc@abc.com")
+                .password("1234")
+                .name("홍길동1")
+                .build();
+
+        Long memberId = memberService.saveMember(memberForm);
+
+        MemberLoginRequest memberLoginRequest = MemberLoginRequest.builder()
+                .email("abc@abc.com")
+                .password("1234")
+                .build();
+
+        // when
+        MemberForm loginMember = memberService.loginMember(memberLoginRequest);
+
+        // then
+        assertThat(loginMember.getId()).isEqualTo(memberId);
+        assertThat(loginMember.getName()).isEqualTo(memberForm.getName());
     }
 
     @Test
